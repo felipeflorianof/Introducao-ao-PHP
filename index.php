aa5860f27ea38ade2ed851039374957d6064a395
Felipeflorianof <felipeflorianof@gmail.com>
2022-05-12 00:15:12 -0300

    Adicionando exibição de resposta após a validação de dados nos campos do formulário

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,13 +10,25 @@
     <title>HTML&PHP</title>
 </head>
 <body>
-    <p>Formulário para inscrição de Competidores</p>
-
+        <p>Formulário para inscrição de Competidores</p>
+ 
     <form action="script.php" method="post">
-       
+        <?php
+
+        $mensagemDeSucesso = isset($_SESSION['mensagem-de-sucesso']) ? $_SESSION['mensagem-de-sucesso'] : ' ';
+        if(!empty($mensagemDeSucesso)) {
+            echo $mensagemDeSucesso;
+        }
+
+        $mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : ' ';
+        if(!empty($mensagemDeErro)) {
+            echo $mensagemDeErro;
+        }
+        ?>
         <p>Seu nome: <input type="text" name="nome"></p>
         <p>Sua Idade: <input type="text" name="idade"></p>
         <p><input type="submit" value="Enviar Dados do Competidor"></p>
+
     </form>
 </body>
 </html>
--- script.php
+++ script.php
@@ -1,51 +1,85 @@
 <?php
 
+session_start();
+
 $categorias = [];
+$categorias[] = 'NaoApto';
 $categorias[] = 'Infantil';
 $categorias[] = 'Adolescente';
 $categorias[] = 'Adulto';
-;
+$categorias[] = 'Idoso';
+
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+
 if(empty($nome)){
-    echo 'O nome não pode ser vazio';
+    $_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio, por favor preencha-o novamente';
+    header( 'Location: index.php');
     return;
 }
 
-if(strlen($nome) < 3) {
-    echo 'O nome não pode ser menor que 3 carácteres';
+else if(strlen($nome) < 3) {
+$_SESSION['mensagem-de-erro'] = 'Certifique-se de que o campo [nome] foi digitado corretamente. (contendo mais de 2 dois caracteres)';
+header( 'Location: index.php');
     return;
 }
 
-if(strlen($nome) > 30){
-    echo 'O nome não pode ser maior que 30 carácteres';
-    return;
+else if(!is_numeric($idade) || strlen($idade) > 3){
+   $_SESSION['mensagem-de-erro'] = 'Certifique-se de que o campo [idade] foi digitado corretamente, (contendo até 3 dígitos e sendo um valor inteiro)';
+   header( 'Location: index.php');
+   return;
 }
 
-if(!is_numeric($idade) || strlen($idade) > 3){
-    echo 'Informe um número válido no campo [idade]';
-    return;
-}
 
-if($idade >= 6 && $idade <= 12) {
+    else if($idade <= 5) {
     for($i = 0; $i <= count($categorias); $i++) {
-        if($categorias[$i] == 'Infantil') {
-            echo "O/A nadador(a) ".$nome. " compete na categoria infantil.";
+        if($categorias[$i] == 'NaoApto'){
+
+            $_SESSION['mensagem-de-sucesso'] = 'O nadador '.$nome.' não pode competir por ser muito novo, só aceitamos competidores com 6 anos ou mais.';
+            header('Location: index.php');
+            return;
         }
     }
-}else if($idade >= 13 && $idade <= 17) {
+}
+    if($idade >= 6 && $idade <= 12) {
+    for($i = 0; $i <= count($categorias); $i++) {
+        if($categorias[$i] == 'Infantil') {
+           
+         $_SESSION['mensagem-de-sucesso'] = 'O nadador '.$nome.' compete na categoria infantil';
+         header('Location: index.php');
+         return;
+        }
+    }
+}   
+    else if($idade >= 13 && $idade <= 17) {
     for($i = 0; $i <= count($categorias); $i++) {
         if($categorias[$i] == 'Adolescente') {
-            echo "O/A nadador(a) ".$nome. " compete na categoria Adolescente.";  
-            }
+
+          $_SESSION['mensagem-de-sucesso'] = 'O nadador '.$nome.' compete na categoria Adolescente';
+         header('Location: index.php');
+         return;
+        }
     }
 }
-else{
+    else if($idade >= 18 && $idade <= 80){
     for($i = 0; $i <= count($categorias); $i++) {
         if($categorias[$i] == 'Adulto') {
-            echo "O/A nadador(a) ".$nome. " compete na categoria Adulto.";
+
+            $_SESSION['mensagem-de-sucesso'] = 'O nadador '.$nome.' compete na categoria Adulto';
+         header('Location: index.php');
+         return;
+        }
+    }
+}   
+    else if($idade >= 81) {
+    for($i = 0; $i <= count($categorias); $i++) {
+        if($categorias[$i] == 'Idoso') {
+
+             $_SESSION['mensagem-de-sucesso'] = 'O nadador '.$nome.' não pode competir por questões de segurança relacionada a idade do mesmo.';
+         header('Location: index.php');
+         return;
         }
     }
 }
